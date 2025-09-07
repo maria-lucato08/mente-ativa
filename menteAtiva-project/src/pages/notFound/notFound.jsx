6e766adb17a54a0c35ca3ddfd64b29ccc9862935
maria lucato <maria.lucato@etec.sp.gov.br>
2025-09-07 15:26:58 -0300

    pag notFund finalizada

--- menteAtiva-project/src/pages/notFound/notFound.jsx
+++ menteAtiva-project/src/pages/notFound/notFound.jsx
@@ -3,8 +3,11 @@ import style from "./notFound.module.css";
 const NotFound = () => {
   return (
     <div className={style.notFound}>
-      <h1>Erro 404</h1>
-      <p>Página não encontrada</p>
+      <div className={style.texto}>
+       <h1>Erro 404</h1>
+       <p>Página não encontrada</p>
+      </div>
+      <img src="./assets/imgNotFound.png" alt="computador" width={300} height={300} />
     </div>
   );
 };
